fe57fe014dfda94515b5ccc84328a1304afebe8e
rajanraj2 <rajanraj253545@gmail.com>
2025-03-23 20:14:13 +0530

    Add summary endpoint

--- fastapi-backend/config.py
+++ fastapi-backend/config.py
@@ -1,1 +1,2 @@
 MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"  # Lightweight SBERT model
+BART_MODEL_NAME = "facebook/bart-large-cnn"  # Large BART model
--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from routes.evaluate import router as evaluate_router
 from routes.transcribe import router as transcription_router
+from routes.summary_feedback import router as summary_feedback_router
+from routes.summary import router as summary_router
 
 app = FastAPI(title="AI Evaluation Service", version="1.0")
 
 # Include API routes
 app.include_router(evaluate_router, prefix="/api/evaluate")
 app.include_router(transcription_router, prefix="/api")
+# app.include_router(summary_feedback_router, prefix="/api/summary_feedback")
+app.include_router(summary_router, prefix="/api/summary")
+
 
 @app.get("/")
 def home():
     return {"message": "AI Evaluation API is running!"}
+
