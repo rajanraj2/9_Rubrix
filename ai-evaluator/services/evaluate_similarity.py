2533c1288d91df1e3e87ec4cb8cab84db3c7d909
rajanraj2 <rajanraj253545@gmail.com>
2025-03-26 13:34:27 +0530

    Add more details for swagger ui

--- ai-evaluator/services/evaluate_similarity.py
+++ ai-evaluator/services/evaluate_similarity.py
@@ -21,24 +21,3 @@ def compute_similarity(problem_statement: str, student_text: str):
         "student_embedding": student_embedding.tolist()
     }
 
-
-
-# import numpy as np
-# from sklearn.metrics.pairwise import cosine_similarity
-# from models.sbert_model import sbert_model
-
-# def generate_embedding(text: str):
-#     """Generate SBERT embedding for a given text."""
-#     return sbert_model.encode(text, convert_to_numpy=True)
-
-# def compute_similarity(student_text: str, ideal_embedding):
-#     """Compute cosine similarity between student and pre-stored ideal solution embeddings."""
-#     student_embedding = generate_embedding(student_text)
-
-#     similarity = cosine_similarity([student_embedding], [np.array(ideal_embedding)])[0][0]
-
-#     return {
-#         "similarity_score": round(float(similarity * 100), 2),
-#         "student_embedding": student_embedding.tolist()  # Convert NumPy array to list for JSON
-#     }
-
